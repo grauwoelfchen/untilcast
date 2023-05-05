c36fee5f5c4d92175a4472701c3e3f91a3d530c0
Yasuhiro Яша Asaka <yasuhiro.asaka@grauwoelfchen.net>
2023-05-05 14:19:42 +0900

    Wrap a long line

--- src/Pages/About.elm
+++ src/Pages/About.elm
@@ -13,10 +13,16 @@ page =
       { title = "About - Until"
       , body =
         [ H.text "/about"
-        , H.p [] [ H.text "Until — untilcats.ch (yes, it's *cats*, not cast. I just pretended a cute typo ha ha) — is an experimental space for my miscellaneous UNuttered TIL (Today I Learnt) screencasts. Let's see what I can :)" ]
+        , H.p [] [ H.text """
+Until — untilcats.ch (yes, it's *cats*, not cast.
+I just pretended a cute typo ha ha) — is an experimental space for
+my miscellaneous UNuttered TIL (Today I Learnt) screencasts.
+Let's see what I can :)
+""" ]
         , H.p [] [ H.text "I'm very whimsical. Miaow!!" ]
         , H.p [] [
-            H.a [ A.href "https://gitlab.com/grauwoelfchen/until" ] [ H.text "Source code" ]
+            H.a [ A.href "https://gitlab.com/grauwoelfchen/until" ]
+              [ H.text "Source code" ]
           ]
         ]
       }
