0127a5d1f3a0db54f315c551eea39abae9b99db9
Yasuhiro Яша Asaka <yasuhiro.asaka@grauwoelfchen.net>
2023-05-05 23:32:09 +0900

    Update about page

--- src/Pages/About.elm
+++ src/Pages/About.elm
@@ -13,13 +13,17 @@ page =
       { title = "About - Until"
       , body =
         [ H.text "/about"
-        , H.p [] [ H.text """
-Until — untilcats.ch (yes, it's *cats*, not cast.
+        , H.p []
+            [ H.span [] [ H.text "Until" ]
+            , H.text """
+— untilcats.ch (yes, it's *cats*, not cast.
 I just pretended to make a cute typo ha ha) — is an experimental space for
 my miscellaneous UNuttered TIL (Today I Learnt) screencasts.
-Let's see what I can :)
-""" ]
+Let's see what I can do :)
+"""
+            ]
         , H.p [] [ H.text "I'm very whimsical. Miaow!!" ]
+        , H.p [] [ H.text "#HackingInTheVoid" ]
         , H.p [] [
             H.a [ A.href "https://gitlab.com/grauwoelfchen/until" ]
               [ H.text "Source code" ]
